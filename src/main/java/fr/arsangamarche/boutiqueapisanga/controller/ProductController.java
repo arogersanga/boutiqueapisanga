e45e0bdc6e1374d7695a4e32deda95915010bfa3
Alain Roger SANGA <ar.sanga@sciencesu-lyon.fr>
2023-03-10 00:31:52 +0100

    resolve cors origin

--- src/main/java/fr/arsangamarche/boutiqueapisanga/controller/ProductController.java
+++ src/main/java/fr/arsangamarche/boutiqueapisanga/controller/ProductController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,6 +19,7 @@ import fr.arsangamarche.boutiqueapisanga.serviceImpl.ProductServiceImpl;
 
 @Controller
 @RequestMapping(path="/")
+@CrossOrigin(origins = "*", maxAge = 3600)
 public class ProductController {
 	
 	Product product;
